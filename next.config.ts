93341ca4a6392265fe6f9a29225bdd47adb905f4
nisalrenuja <nrenuja@gmail.com>
2026-01-10 22:48:44 +0530

    Add GitHub Pages deployment workflow

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
